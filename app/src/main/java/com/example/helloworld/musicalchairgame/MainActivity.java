35440df53ba5e1e83a3ba39c83ab3ccc35f2525e
Shailesh Gupta <shailesh.gupta@flipkart.com>
2016-01-11 23:45:55 +0530

    playing song from sd card

--- app/src/main/java/com/example/helloworld/musicalchairgame/MainActivity.java
+++ app/src/main/java/com/example/helloworld/musicalchairgame/MainActivity.java
@@ -1,47 +1,133 @@
 package com.example.helloworld.musicalchairgame;
 
-import android.app.Activity;
-import android.content.ContentResolver;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Bundle;
-import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
 
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.database.Cursor;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+/*
+ * This is demo code to accompany the Mobiletuts+ series:
+ * Android SDK: Creating a Music Player
+ *
+ * Sue Smith - February 2014
+ */
+
 public class MainActivity extends Activity {
 
+    //song list variables
     private ArrayList<Song> songList;
     private ListView songView;
+
+    //service
+    private MusicService musicSrv;
+    private Intent playIntent;
+    //binding
+    private boolean musicBound=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        songList = new ArrayList<Song>();
         setContentView(R.layout.activity_main);
+
+        //retrieve list view
+        songView = (ListView)findViewById(R.id.song_list);
+        //instantiate list
+        songList = new ArrayList<Song>();
+        //get songs from device
         getSongList();
-        Collections.sort(songList, new Comparator<Song>() {
-            public int compare(Song a, Song b) {
+        //sort alphabetically by title
+        Collections.sort(songList, new Comparator<Song>(){
+            public int compare(Song a, Song b){
                 return a.getTitle().compareTo(b.getTitle());
             }
         });
-
+        //create and set adapter
         SongAdapter songAdt = new SongAdapter(this, songList);
-        songView = (ListView)findViewById(R.id.song_list);
         songView.setAdapter(songAdt);
-
     }
 
-    public void getSongList() {
-        //retrieve song info
+    //connect to the service
+    private ServiceConnection musicConnection = new ServiceConnection(){
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            //get service
+            musicSrv = binder.getService();
+            //pass list
+            musicSrv.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicBound = false;
+        }
+    };
+
+    //start and bind the service when the activity starts
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(playIntent==null){
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
+
+    //user song select
+    public void songPicked(View view){
+        musicSrv.setSong(Integer.parseInt(view.getTag().toString()));
+        musicSrv.playSong();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //menu item selected
+//        switch (item.getItemId()) {
+//            case R.id.action_shuffle:
+//                //shuffle
+//                break;
+//            case R.id.action_end:
+//                stopService(playIntent);
+//                musicSrv=null;
+//                System.exit(0);
+//                break;
+//        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //method to retrieve song info from device
+    public void getSongList(){
+        //query external audio
         ContentResolver musicResolver = getContentResolver();
         Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
-
+        //iterate over results if valid
         if(musicCursor!=null && musicCursor.moveToFirst()){
             //get columns
             int titleColumn = musicCursor.getColumnIndex
@@ -61,5 +147,11 @@ public class MainActivity extends Activity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        stopService(playIntent);
+        musicSrv=null;
+        super.onDestroy();
+    }
 
 }
